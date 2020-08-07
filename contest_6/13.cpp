cb580b0079971e7146365f995af61e69040853d5
tuancris <62863015+tuancris@users.noreply.github.com>
2020-08-07 15:24:48 +0700

    Update 13.cpp

--- contest_6/13.cpp
+++ contest_6/13.cpp
@@ -2,21 +2,18 @@
 #define max 100000
 using namespace std;
 
-int b[max];
-int dem(int a[],int n,int x){
-    for(int i=0; i<max; i++)
-        b[i]=0;
-    for(int i=0; i<n; i++)
-        b[a[i]]++;
-    if(b[x]==0) return -1;
-    else return b[x];
-}
 int main() {
     int t; cin >> t;
     while(t--) {
-        int n,x; cin >> n >> x; int a[n];
+        int n, x; cin >> n >> x;
+        int a[n];
         for(int i=0; i<n; i++) cin >> a[i];
-        cout << dem(a,n,x) << endl;
+        int dem = 0;
+        for(int i=0; i<n; i++) {
+            if(a[i] == x) dem++;
+        }
+        if(dem == 0) cout << "-1" << endl;
+        else cout << dem << endl;
     }
     return 0;
 }
